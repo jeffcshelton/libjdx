06ff0c9029464a45ed447818d738178192eef829
jeffreycshelton <jeffrey.conner.shelton@gmail.com>
2021-08-27 10:57:19 -0400

    Create function to write JDX to files

--- src/obj.c
+++ src/obj.c
@@ -1,6 +1,7 @@
 #include "jdx/jdx.h"
 
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <libdeflate.h>
@@ -85,6 +86,76 @@ JDXObject JDX_ReadObjectFromPath(const char *path) {
 
     fclose(file);
     return obj;
+}
+
+void JDX_WriteObjectToFile(JDXObject obj, FILE *file) {
+    // TODO: Reconsider implentation
+    JDXColorType color_type = obj.images[0].color_type;
+    const char *color_signature = NULL;
+    
+    if (color_type == JDXColorType_GRAY) {
+        color_signature = "GRAY";
+    } else if (color_type == JDXColorType_RGB) {
+        color_signature = "RGB8";
+    } else if (color_type == JDXColorType_RGBA) {
+        color_signature = "RGBA";
+    } else {
+        errno = EINVAL;
+        return;
+    }
+
+    // Write string "JDX" followed by version info
+    fwrite("JDX", 1, 3, file);
+    fwrite(&obj.version.major, 1, 1, file);
+    fwrite(&obj.version.minor, 1, 1, file);
+    fwrite(&obj.version.patch, 1, 1, file);
+
+    // Write the color signature, image width and height, and image count
+    fwrite(color_signature, 1, 4, file);
+    fwrite(&obj.images[0].width, sizeof(int16_t), 1, file);
+    fwrite(&obj.images[0].height, sizeof(int16_t), 1, file);
+    fwrite(&obj.item_count, sizeof(int64_t), 1, file);
+
+    size_t image_size = obj.images[0].width * obj.images[0].height * obj.images[0].color_type;
+    size_t uncompressed_size = obj.item_count * (image_size + 2);
+
+    uint8_t *uncompressed_body = malloc(uncompressed_size);
+    uint8_t *body_ptr = uncompressed_body;
+
+    for (int i = 0; i < obj.item_count; i++) {
+        memcpy(body_ptr, obj.images[i].data, image_size);
+        body_ptr += image_size;
+
+        *((JDXLabel *) body_ptr) = obj.labels[i];
+        body_ptr += sizeof(JDXLabel);
+    }
+
+    // Must allocate for entire uncompressed size despite it almost certainly being less
+    uint8_t *compressed_body = malloc(uncompressed_size);
+
+    struct libdeflate_compressor *compressor = libdeflate_alloc_compressor(12);
+    int64_t compressed_size = libdeflate_deflate_compress(
+        compressor,
+        uncompressed_body,
+        uncompressed_size,
+        compressed_body,
+        uncompressed_size
+    );
+
+    libdeflate_free_compressor(compressor);
+
+    // libdeflate will return 0 if operation failed
+    if (compressed_size == 0) {
+        errno = EPROTO;
+        return;
+    }
+
+    fwrite(&compressed_size, sizeof(int64_t), 1, file);
+    fwrite(compressed_body, 1, compressed_size, file);
+    fclose(file);
+
+    free(uncompressed_body);
+    free(compressed_body);
 }
 
 void JDX_FreeObject(JDXObject obj) {
