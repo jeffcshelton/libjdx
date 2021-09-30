6eade2008fa0a11c29bf171d3eb6dc60a855049e
jeffreycshelton <jeffrey.conner.shelton@gmail.com>
2021-09-30 08:49:55 -0400

    Simplify construct_error_header to construct_error

--- src/header.c
+++ src/header.c
@@ -5,14 +5,14 @@
 
 const JDXVersion JDX_VERSION = { 0, 1, 0 };
 
-static inline JDXHeader construct_error_header(const char *error) {
-    JDXHeader error_header = {
+static inline JDXHeader construct_error(const char *error_msg) {
+    JDXHeader error_struct = {
         { -1, -1, -1 },
         -1, -1, -1, -1, -1,
-        error
+        error_msg
     };
 
-    return error_header;
+    return error_struct;
 }
 
 JDXHeader JDX_ReadHeaderFromFile(FILE *file) {
@@ -20,7 +20,7 @@ JDXHeader JDX_ReadHeaderFromFile(FILE *file) {
     fread(corruption_check, sizeof(corruption_check), 1, file);
 
     if (memcmp(corruption_check, "JDX", 3) != 0)
-        return construct_error_header("corruption check failed");
+        return construct_error("corruption check failed");
 
     JDXHeader header;
     char color_signature[4];
@@ -33,7 +33,7 @@ JDXHeader JDX_ReadHeaderFromFile(FILE *file) {
     fread(&header.body_size, sizeof(header.body_size), 1, file);
 
     if (header.image_width < 0 || header.image_height < 0 || header.image_count < 0 || header.body_size < 0)
-        return construct_error_header("invalid header item");
+        return construct_error("invalid header item");
 
     JDXColorType color_type;
     if (memcmp(color_signature, "RGB8", 4) == 0) {
@@ -43,7 +43,7 @@ JDXHeader JDX_ReadHeaderFromFile(FILE *file) {
     } else if (memcmp(color_signature, "GRAY", 4) == 0) {
         color_type = JDXColorType_GRAY;
     } else {
-        return construct_error_header("invalid color signature");
+        return construct_error("invalid color signature");
     }
 
     header.color_type = color_type;
@@ -55,7 +55,7 @@ JDXHeader JDX_ReadHeaderFromPath(const char *path) {
     FILE *file = fopen(path, "rb");
 
     if (file == NULL)
-        return construct_error_header("cannot open file");
+        return construct_error("cannot open file");
 
     JDXHeader header = JDX_ReadHeaderFromFile(file);
 
