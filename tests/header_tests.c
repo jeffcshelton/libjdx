03cdfd44abadbf8c9c566af0972263a24fea2df8
jeffreycshelton <jeffrey.shelton.dev@gmail.com>
2022-02-16 11:21:25 -0500

    Change header tests to use JDXHeader object ptr

--- tests/header_tests.c
+++ tests/header_tests.c
@@ -14,17 +14,17 @@ TEST_FUNC(CompareVersions) {
 }
 
 TEST_FUNC(ReadHeaderFromPath) {
-	JDXHeader header;
-	JDXError error = JDX_ReadHeaderFromPath(&header, "./res/example.jdx");
+	JDXHeader *header = JDX_AllocHeader();
+	JDXError error = JDX_ReadHeaderFromPath(header, "./res/example.jdx");
 
 	final_state = (
 		error == JDXError_NONE &&
-		JDX_CompareVersions(header.version, JDX_VERSION) == 0 &&
-		header.bit_depth == 24 &&
-		header.image_width == 52 &&
-		header.image_height == 52 &&
-		header.item_count == 8
+		JDX_CompareVersions(header->version, JDX_VERSION) == 0 &&
+		header->bit_depth == 24 &&
+		header->image_width == 52 &&
+		header->image_height == 52 &&
+		header->item_count == 8
 	) ? STATE_SUCCESS : STATE_FAILURE;
 
-	JDX_FreeHeader(&header);
+	JDX_FreeHeader(header);
 }
